fd7010db3e23d3082b727ecb8af4be41517e8bda
INFORGi <147039592+INFORGi@users.noreply.github.com>
2025-03-15 12:07:01 +0500

    Commit

--- Pipeline.py
+++ Pipeline.py
@@ -1,71 +1,108 @@
 import cv2
 import numpy as np
 from ultralytics import YOLO
-import matplotlib.pyplot as plt
-#ddf
-def detect_objects(image_path):
-    # Load YOLO model
-    model = YOLO('yoloWorld.pt')
+import io
+from PIL import Image
+from googletrans import Translator
+import piexif
 
-    image = cv2.imread(image_path)
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    results = model(image_rgb)[0]
-    annotated_image = image_rgb.copy()
+class ImageProcessor:
+    def __init__(self):
+        self.model = YOLO('yolov8x-worldv2.pt')
+        self.translator = Translator()
 
-    np.random.seed(42)
-    colors = np.random.randint(0, 255, size=(100, 3), dtype=np.uint8)
+    def detect_objects(self, image_bytes):
+        image = Image.open(io.BytesIO(image_bytes))
+        image_rgb = np.array(image.convert("RGB"))
 
-    class_labels = {}
+        results = self.model(image_rgb)[0]
+        annotated_image = image_rgb.copy()
 
-    boxes = results.boxes
+        np.random.seed(42)
+        colors = np.random.randint(0, 255, size=(100, 3), dtype=np.uint8)
 
-    return boxes, results.names, annotated_image, colors
+        boxes = results.boxes
 
-def show_results(image_path, confidence_threshold):
+        return boxes, results.names, annotated_image, colors
 
-    original_image = cv2.imread(image_path)
-    original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+    def add_metadata_to_image(self, image_bytes, detections, original_image_bytes):
+        class_names_str = ", ".join(detections["objects"])
+        print(f"Detected objects: {class_names_str}")
 
-    boxes, class_names, annotated_image, colors = detect_objects(image_path)
+        image = Image.open(io.BytesIO(image_bytes))
 
-    class_labels = {}
-    for box in boxes:
-        x1, y1, x2, y2 = map(int, box.xyxy[0])
+        original_image = Image.open(io.BytesIO(original_image_bytes))
+        exif_data = original_image.info.get("exif", None)
 
-        confidence = float(box.conf[0])
+        if exif_data:
+            overwriting_meta = piexif.load(exif_data)
+        else:
+            overwriting_meta = piexif.load(piexif.dump({}))
 
-        if confidence > confidence_threshold:
-            class_id = int(box.cls[0])
-            class_name = class_names[class_id]
+        print(f"EXIF before: {overwriting_meta}")
 
-            color = colors[class_id % len(colors)].tolist()
+        overwriting_meta["0th"][piexif.ImageIFD.Make] = class_names_str.encode('utf-8')
 
-            cv2.rectangle(annotated_image, (x1, y1), (x2, y2), color, 2)
-            class_labels[class_name] = color
+        print(f"EXIF after: {overwriting_meta}")
 
-            # Добавляем жирный текст, рисуя несколько слоев
-            font_scale = 3  # Увеличиваем размер шрифта
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            thickness = 3  # Увеличиваем толщину для жирности
+        exif_bytes = piexif.dump(overwriting_meta)
 
-            # Создаем эффект жирного текста, рисуя несколько слоев с небольшими смещениями
-            for offset in range(-1, 2):
-                cv2.putText(annotated_image, class_name, (x1 + offset, y1 - 10 + offset), font, font_scale, color, thickness)
+        img_byte_arr = io.BytesIO()
+        image.save(img_byte_arr, format='JPEG', exif=exif_bytes)
+        return img_byte_arr.getvalue()
 
-    plt.figure(figsize=(15, 7))
-    
-    plt.subplot(1, 2, 1)
-    plt.title('Original Image')
-    plt.imshow(original_image)
-    plt.axis('off')
-    
-    plt.subplot(1, 2, 2)
-    plt.title('Detected Objects')
-    plt.imshow(annotated_image)
-    plt.axis('off')
+    def process_image(self, image_bytes, confidence_threshold=0.2):
+        original_image = Image.open(io.BytesIO(image_bytes))
+        original_image_rgb = np.array(original_image.convert("RGB"))
 
-    plt.tight_layout()
-    plt.show()
+        boxes, class_names, annotated_image, colors = self.detect_objects(image_bytes)
 
-show_results('88.jpg', confidence_threshold=0.2)
+        detections = {"objects": []}
+
+        for box in boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+
+            confidence = float(box.conf[0])
+            if confidence > confidence_threshold:
+                class_id = int(box.cls[0])
+                class_name = class_names[class_id]
+
+                translated_class_name = self.translator.translate(class_name, src='en', dest='ru').text
+
+                color = colors[class_id % len(colors)].tolist()
+                cv2.rectangle(annotated_image, (x1, y1), (x2, y2), color, 2)
+
+                font_scale = 3
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                thickness = 10
+
+                for offset in range(-1, 2):
+                    cv2.putText(annotated_image, translated_class_name, (x1 + offset, y1 - 10 + offset), font, font_scale, color, thickness)
+
+                detections["objects"].append(translated_class_name)
+                
+
+        img_byte_arr = io.BytesIO()
+        Image.fromarray(annotated_image).save(img_byte_arr, format='JPEG')
+
+        annotated_image_bytes = img_byte_arr.getvalue()
+
+        # Добавляем метаданные с информации о классах объектов
+        updated_image_bytes = self.add_metadata_to_image(annotated_image_bytes, detections, image_bytes)
+        
+        return updated_image_bytes
+
+    def get_image_with_annotations(self, image_bytes, confidence_threshold=0.2):
+        return self.process_image(image_bytes, confidence_threshold)
+
+
+processor = ImageProcessor()
+
+with open('88.jpg', 'rb') as f:
+    image_bytes = f.read()
+
+updated_image_bytes = processor.get_image_with_annotations(image_bytes)
+
+with open('88-р.jpg', 'wb') as f:
+    f.write(updated_image_bytes)
